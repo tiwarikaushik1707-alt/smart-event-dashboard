b1e251a761813ce60cfb40f591e205d5d7dc208c
tiwarikaushik1707-alt <tiwarikaushik1707@gmail.com>
2026-02-16 08:52:05 +0530

    event creation

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("JS connected");
+const form = document.getElementById("eventForm");
+const eventList = document.getElementById("eventList");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const div = document.createElement("div");
+  div.textContent = "New Event Added";
+  eventList.appendChild(div);
+});
